c2c88319b693d159e9813d14eb80d1fb2bf8c515
Raju204116 <raju204116@gmail.com>
2026-07-15 23:16:01 +0600

    js-numbers

---
+++ Module-7-Intro to JS/c7-numbers.js
@@ -0,0 +1,32 @@
+
+/*
+✅Numbers in JavaScript
+
+A Number is a data type used to store numeric values.
+
+let age = 25;
+let price = 99.99;
+let temperature = -5;
+
+All of these are of type number.
+
+
+✅Types of Numbers
+
+1. Integer : A whole number (no decimal point).
+
+let age = 25;
+let year = 2026;
+let score = -10;
+
+
+2. Floating-Point Number (Decimal) : A number with a decimal point.
+
+let price = 99.99;
+let pi = 3.14159;
+let height = 5.8;
+
+
+
+
+*/
