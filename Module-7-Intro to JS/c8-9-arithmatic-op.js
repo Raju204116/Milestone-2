1a7efa31cc292fa4adabf37befaa34e29fa45cbc
Raju204116 <raju204116@gmail.com>
2026-07-15 23:26:45 +0600

    arithmatic-operations

---
+++ Module-7-Intro to JS/c8-9-arithmatic-op.js
@@ -0,0 +1,81 @@
+
+/*
+✅Arithmetic Operators
+
+JavaScript supports basic math operations.
+
+| Operator | Meaning             | Example  |
+| -------- | ------------------- | -------- |
+| `+`      | Addition            | `10 + 5` |
+| `-`      | Subtraction         | `10 - 5` |
+| `*`      | Multiplication      | `10 * 5` |
+| `/`      | Division            | `10 / 5` |
+| `%`      | Remainder (Modulus) | `10 % 3` |
+| `**`     | Exponent (Power)    | `2 ** 3` |
+
+//Increment and Decrement operator :
+
+let count = 5;
+
+count++;
+count--;
+
+console.log(count);
+
+
+//shorthand operator : 
+
+
+let x = 10;
+
+x += 5; // x = x + 5
+console.log(x); // 15
+
+x -= 3; // x = x - 3
+console.log(x); // 12
+
+x *= 2; // x = x * 2
+console.log(x); // 24
+
+x /= 4; // x = x / 4
+console.log(x); // 6
+
+
+
+
+✅JavaScript Math Object
+JavaScript provides many built-in math methods.
+
+1.Math.round() : Rounds to the nearest whole number.
+2.Math.floor() : Rounds down.
+3.Math.ceil() : Rounds up.
+4.Math.random(): Generates a random number between 0 (inclusive) and 1 (exclusive).
+5.Math.max() and Math.min() 
+6.Math.sqrt() : Square root.
+7.Math.pow(): Power of a number.
+
+
+
+✅Exceptional operations :
+
+1. Mixing strings and numbers
+console.log("10" + 5); // 105
+
+2. Division by zero 
+console.log(10 / 0);  //infinity
+
+
+3. Floating-point precision
+console.log(0.1 + 0.2);  //0.30000000000000004
+
+4.Pre & post  Increment 
+
+let price =400;
+console.log(price++); //400
+console.log(price); //401
+
+let price =500;
+console.log(++price); //501
+console.log(price); //501
+
+*/
