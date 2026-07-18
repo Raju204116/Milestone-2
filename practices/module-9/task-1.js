5e9348508afda9bf47ebb59a4b241cef7307d6b8
Raju204116 <raju204116@gmail.com>
2026-07-18 18:35:02 +0600

    module-9-task-9-completed

---
+++ practices/module-9/task-1.js
@@ -0,0 +1,147 @@
+
+//✅Task 1 — Print "Hello" Five Times
+
+// for(let i=0;i<5;i++){
+//     console.log("Hello");
+// }
+
+
+//✅Task 2 — Print Numbers 1 to 10
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+
+
+//✅Task 3 — Countdown with While Loop 
+// Write a while loop that prints numbers from 10 down to 1.
+
+// let i=10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+
+
+//✅Task 4 — Repeat a Message
+//Using a while loop, print "Loop চলছে " exactly 7 times
+
+// let count=0;
+// while(count<7){
+//     console.log("Loop চলছে");
+//     count++;
+// }
+
+
+//✅Task 5 — Even Numbers
+// Write a for loop that prints all even numbers between 1 and 30
+
+// for(let i=0; i<=30; i=i+2){
+//     console.log("Even Numbers : ",i);
+// }
+
+//best method
+// for(let i=1; i<=30; i++){
+//     if(i%2===0){
+//         console.log("Even Numbers : ",i);
+//     }
+// }
+
+// console.log(0%2===0)
+
+
+
+// ✅Task 6 — Sum of Numbers
+// Write a for loop that calculates the sum of numbers from 1 to 20 and prints the result
+
+// let sum =0;
+// for(let i=1;i<=20;i++){
+//     sum +=i;   
+// }
+// console.log("Sum:",sum);
+
+
+
+// ✅Task 7 — Multiplication Table
+// Ask the user for a number (or set a variable), then print its multiplication table from 1 to 10 using a for loop.
+// Example: if the number is 7 → 7 x 1 = 7, 7 x 2 = 14, ... 7 x 10 = 70
+
+
+// let num = Number(prompt("Enter a number from 1 to 10: "));
+//prompt is only works for browser, not in node.js
+
+
+// let num=7;
+// for(let i=1;i<=10;i++){
+//     console.log(`${num} * ${i} = ${num * i }`);
+// }
+
+
+
+
+
+// ✅Task 8 — Count Down from 20
+// Write a for loop that counts down from 20 to 1.
+
+// for(let i=20; i>0; i--){
+//     console.log(i);
+// }
+
+
+
+
+
+// ✅Task 9 — Sum of Even Numbers
+// Write a for loop that calculates the sum of all even numbers from 2 to 50
+
+// let sum=0;
+// for(let i=1; i<=50; i++){
+//     if(i%2===0){
+//         sum +=i;
+//     }
+// }
+// console.log("Sum of Even Num : ", sum);
+
+
+
+
+
+// ✅Task 10 — Using Break
+// Write a for loop from 1 to 50 that stops completely (break) as soon as it reaches a
+// number greater than 30
+
+
+
+
+
+// ✅Task 11 — Using Continue
+// Write a for loop from 1 to 20 that skips (continue) any number divisible by 4.
+
+// ✅Task 12 — Do While Loop — Optional
+// Write a do while loop that prints numbers from 1 to 5. Then modify the condition so it is
+// false from the start (for example, i > 10) and observe that the loop still runs once
+
+
+
+// ✅Task 13 — Comparing Loop Types
+// Write the same task — printing numbers 1 to 5 — three times: once using for, once using
+// while, and once using do while. Compare how the code structure differs for each
+
+
+// ✅Task 14 — Mini Challenge
+// Write a program that:
+// 1. Loops from 1 to 100.
+// 2. Skips (continue) any number divisible by 5.
+// 3. Stops (break) completely once it reaches a number greater than 40.
+// 4. Prints every number that passes both checks.
+
+
+// ✅
+
+
+
+
+
+
