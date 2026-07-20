c9163b0de1cdbd48cb1fa0896b9a17bb675cc2fe
Raju204116 <raju204116@gmail.com>
2026-07-20 19:41:58 +0600

    c1-2-3-completed

---
+++ Module-10-array.js/c1-2-array-basic.js
@@ -0,0 +1,71 @@
+
+/* 
+✅What is an Array?
+
+An array is a special object that stores multiple values in a single variable.
+
+Instead of writing:
+
+let student1 = "Raju";
+let student2 = "Rahim";
+let student3 = "Karim";
+
+Use an array:
+let students = ["Raju", "Rahim", "Karim"];
+
+
+✅Why Do We Need Arrays?
+
+Suppose you have the marks of 100 students.
+
+Without an array:
+
+let mark1 = 80;
+let mark2 = 75;
+let mark3 = 90;
+// ...
+
+With an array:
+let marks = [80, 75, 90];
+
+One variable can hold many values.
+
+
+✅Creating an Array
+
+Numbers
+let nums = [10, 20, 30, 40];
+
+Strings
+let fruits = ["Apple", "Banana", "Mango"];
+
+Mixed Data Types
+let data = [10, "Raju", true, null];
+
+
+✅Array Index
+
+Each element has an index.
+
+Index :   0      1      2
+Value : ["A",  "B",  "C"]
+
+The first element is always at index 0.
+
+
+
+
+✅
+✅
+✅
+✅
+
+
+
+
+
+
+
+
+
+*/
---
+++ Module-10-array.js/c3-get-set-length.js
@@ -0,0 +1,55 @@
+
+/* 
+✅Accessing Elements (get)
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+Output:
+
+Apple
+Banana
+Mango
+
+✅Changing Elements (set)
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+fruits[1] = "Orange";
+console.log(fruits);
+
+Output:
+
+["Apple", "Orange", "Mango"]
+
+
+✅Array Length
+
+let nums = [10, 20, 30, 40];
+console.log(nums.length);
+
+Output:4
+
+
+
+✅Last Element
+
+Instead of writing:
+nums[3]
+
+Use:
+nums[nums.length - 1]
+
+Example:
+
+let nums = [10, 20, 30, 40];
+console.log(nums[nums.length - 1]);
+
+Output:40
+
+
+
+*/
