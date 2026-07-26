18d499adf1bae07e0a92f3ecfbd34cab1ec4a266
Raju204116 <raju204116@gmail.com>
2026-07-26 20:28:30 +0600

    Module-13-class-added

---
+++ Module-13-problem-solving/c1-mindset.js
@@ -0,0 +1,14 @@
+
+console.log("3-step to solve a problem : ");
+let problem ={
+    step1 : "Understand",
+    step2 : "Breakdown into small part",
+    step3 : "Plan (which loop/func will need)",
+
+    tip1: "Do this 3 before touching the keyboard.",
+    tip2: "Problem = Understanding(70%) + code(30%) => solved "
+}
+console.log(problem);
+
+
+
