e6bc7801fb0b87f2fafbe279a8ec2ab1c281676c
Raju204116 <raju204116@gmail.com>
2026-07-28 17:04:59 +0600

    task-5-completed

--- practices/module-13/task-5.js
+++ practices/module-13/task-5.js
@@ -4,9 +4,28 @@ Write a function isPalindrome that takes a string and returns true if it reads
 the same forwards and backwards, otherwise false. Assume lowercase, no
 spaces. */
 
+// input: a string
+// output: true/false
+// return: a boolean 
 
+function isPalindrome(str){
+    let reversedStr= "";
+    for(let letters of str){
+        reversedStr =  letters + reversedStr;
+    }
+    // console.log(reversedStr);
+    if(str===reversedStr){
+        return true;
+    }else
+        return false;
+}
+if(isPalindrome("eye") === true){
+    console.log("The string is Palindrome.");  
+}else
+    console.log("The string is not Palindrome.");
 
-
+console.log(isPalindrome("level")); // Expected: true
+console.log(isPalindrome("hello")); // Expected: false    
 
 
 
@@ -15,3 +34,22 @@ Matches lesson: 13-5 (String problems — split + loops)
 Problem Statement:
 Write a function findShortestWord that takes a sentence and returns the
 shortest word in it. */
+
+function findShortestWord(str){
+    let splitStr= str.split(" ");
+    // console.log(splitStr);
+    // console.log(splitStr[2].length); //4
+
+    let shortestWord=splitStr[0];
+    for(let i=1;i<splitStr.length;i++){
+        if( splitStr[i].length < shortestWord.length){
+            shortestWord=splitStr[i];
+            // console.log(i,splitStr[i],shortestWord);
+        }
+            
+    }
+    return shortestWord;  
+}
+console.log("The shortest Word is :",findShortestWord("we are hero"));
+console.log(findShortestWord("JavaScript is a fun language")); //Expected: "a"
+console.log(findShortestWord("I love JavaScript, a lot!")); //I,a =>I
